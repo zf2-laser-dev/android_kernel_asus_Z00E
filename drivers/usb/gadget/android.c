612e0554f2c863a0e0ce0b8b15d74d29905f0619
Sujeet Kumar <ksujeet@codeaurora.org>
2014-12-05 11:22:22 +0530

    USB: android: Enable streaming only during composition enable
    ADB may reset the connection by writing into EP0 file
    on every disconnect. unbind config disables the streaming
    enabled flag. On next USB connect streaming mode is not
    enabled because the flag was disabled already.
    
    Enable streaming mode during composition enable and mark
    the streaming enabled flag just before that. If the interface
    needs to enable streaming mode then it is enabled at that
    time.
    
    Change-Id: I22eb5e6c7de50e7a81ab29296ca96b97229cc9cf
    Signed-off-by: Sujeet Kumar <ksujeet@codeaurora.org>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -3243,6 +3243,7 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 		printk("[USB] Connect to Other\n");
 	}
 
+	dev->cdev->gadget->streaming_enabled = false;
 	while (b) {
 		conf_str = strsep(&b, ":");
 		if (!conf_str)
@@ -3631,10 +3632,6 @@ static void android_unbind_config(struct usb_configuration *c)
 {
 	struct android_dev *dev = cdev_to_android_dev(c->cdev);
 
-	if (c->cdev->gadget->streaming_enabled) {
-		c->cdev->gadget->streaming_enabled = false;
-		pr_debug("setting streaming_enabled to false.\n");
-	}
 	android_unbind_enabled_functions(dev, c);
 }
 
