11b3ec13076571aa35256432f71b8db3a80a4a5d
Mayank Rana <mrana@codeaurora.org>
2015-08-31 12:36:13 -0700

    usb: gadget: android: Use 1024 bytes as MIDI buffer size
    Some UDC driver doesn't allow or throw errors when USB request buffer
    queued is unaligned i.e. buffer size is not equal to wMaxPacketSize or
    multiple of wMaxPacketSize. Fix this issue by using 1024 bytes as MIDI
    buffer size instead of 256 bytes.
    
    Change-Id: Ib3493f219de242be56be41d129d128facfe4a8a5
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -102,7 +102,7 @@ extern struct completion gadget_init;
 /* f_midi configuration */
 #define MIDI_INPUT_PORTS    1
 #define MIDI_OUTPUT_PORTS   1
-#define MIDI_BUFFER_SIZE    256
+#define MIDI_BUFFER_SIZE    1024
 #define MIDI_QUEUE_LENGTH   32
 
 struct android_usb_function {
